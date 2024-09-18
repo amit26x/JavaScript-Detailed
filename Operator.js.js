4cb0c2ed11e3acc6dbe1a8bfb9833b01f741272f
Amit Gupta <amit2606.gupta@gmail.com>
2024-09-18 11:57:36 +0530

    Opreator is Done

---
+++ Operator.js.js
@@ -0,0 +1,86 @@
+// Aithmetic Operators
+
+let a = 24;
+let b = 12;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(a--);
+
+
+
+// Assignment operators
+
+let a = 5
+a += 4;
+console.log(a)
+
+let a = 5;
+a -= 4;
+console.log(a)
+
+
+let a = 5;
+a *= 4;
+console.log(a)
+
+
+let a = 5;
+a %= 4;
+console.log(a)
+
+let a = 5
+a **= 4;
+console.log(a)
+
+
+//Comparison Operators
+
+let a = 5;
+let b = 5;
+console.log(a==b)
+
+let a = 5;
+let b = 5;
+console.log(a!=b)
+
+let a= 5;
+let b = 2;
+console.log(a===b)
+
+let a= 5;
+let b = 2;
+console.log(a!==b)
+
+
+
+// Logical Operator
+
+let a = 6;
+let b = 5;
+let cond1 = a>b;
+let cond2 = a===6;
+console.log("cond1 && cond2=", cond1 && cond2)
+
+
+let a = 6;
+let b = 5;
+let cond1 = a>b;
+let cond2 = a===6;
+console.log("cond1 || cond2=", cond1 && cond2)
+
+
+let a = 6;
+let b = 5;
+
+console.log("!(a<b) =", !(a<b));
+
+let a = 6;
+let b = 5;
+
+console.log("!(a>b) =", !(a>b));
