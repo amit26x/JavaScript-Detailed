e4918639aae5065c5daa3ff75a690d147c90bdd0
Amit Gupta <amit2606.gupta@gmail.com>
2024-09-18 10:28:26 +0530

    Variables is done

---
+++ Variables.js
@@ -0,0 +1,42 @@
+// Variable in javascript
+// Veriables can be declared in 4 way
+// 1(var) 2(let) 3(const) 4(Automatically)
+
+// Using Var
+
+var a = "Hello World";
+var b = 10;
+var c = 12;
+var d = b + c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+// Using let 
+
+let a = "Hello world";
+let b = "Joinning";
+let c = "12";
+let d = b + c ;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// Using Const
+
+const a = "Hello World";
+console.log(a);
+
+const b = 400;
+console.log(b);
+
+const c = "12";
+console.log(c);
+
+const d = b + c;
+console.log(d);
