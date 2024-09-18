6f602b7b04a7ee980c340ba9c1384e4b8ff2011e
Amit Gupta <amit2606.gupta@gmail.com>
2024-09-18 11:04:04 +0530

    DataTypes is Done

---
+++ Datatypes.js
@@ -0,0 +1,40 @@
+// There are two types datatype 
+// 1 (Primitive Datatypes)  2 (Non Primitive Datatype)
+
+// Non Primitive Datatype
+
+const Product = {
+    title:"Ball Pen",
+    rating: 4,
+    offer: 5,
+    Price: 270,
+};
+console.log(Product);
+
+
+const Profile = {
+    userName : "amit266.x",
+    isFollow : false,
+    follwers: 200,
+    following: 300,
+
+};
+console.log(Profile);
+
+
+// Primitive Datatypes
+
+const a = "Amit";
+console.log(typeof a);
+
+const b = 123;
+console.log(typeof b);
+
+const c = false;
+console.log(typeof c);
+
+let d = null
+console.log( d);
+
+const e = undefined;
+console.log(typeof e);
